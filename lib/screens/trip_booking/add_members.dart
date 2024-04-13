781f3df09912624497050604bddc2c2223c5bf95
AnujLo1c <anujlowanshi3@gmail.com>
2024-04-13 10:20:07 +0530

    booking ui complete(80%)

--- lib/screens/trip_booking/add_members.dart
+++ lib/screens/trip_booking/add_members.dart
@@ -1,7 +1,9 @@
 import 'package:flutter/material.dart';
 import 'package:gap/gap.dart';
+import 'package:hum_chale/screens/custom_bottom_nav.dart';
 import 'package:hum_chale/ui/CustomColors.dart';
 import 'package:hum_chale/widget/CustomAppBar.dart';
+import 'package:flutter_slidable/flutter_slidable.dart';
 class AddMembers extends StatefulWidget {
   static var routeName = "Add-members-screen";
   const AddMembers({super.key});
@@ -11,14 +13,15 @@ class AddMembers extends StatefulWidget {
 }
 
 class _AddMembersState extends State<AddMembers> {
-  List<dynamic> members=[];
+  List<Member> members=[];
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: CustomAppBar(),
       body: Column(
         children: [
-          Gap(20),
+          Gap(40),
           Expanded(
             child: ListView.builder(
               itemCount: members.length,
@@ -32,8 +35,7 @@ class _AddMembersState extends State<AddMembers> {
             width: MediaQuery.of(context).size.width-50,
             margin: EdgeInsets.only(bottom: 17),
             child: ElevatedButton(
-              // onPressed: () => Navigator.pushNamed(context, TripTransit.routeName),
-              onPressed: (){},
+              onPressed: ()=>Navigator.popUntil(context, (route) => route.settings.name==CustomBottomNav.routeName),
               style: ElevatedButton.styleFrom(
                 elevation: 5,
                 shape: const RoundedRectangleBorder(borderRadius: BorderRadius.all(Radius.circular(15))),
@@ -46,7 +48,7 @@ class _AddMembersState extends State<AddMembers> {
         ],
       ),
       floatingActionButton: FloatingActionButton(
-        onPressed: (){}// => AddItem()
+        onPressed: () => AddMember()
         ,child: Icon(
           Icons.add,
           color: Colors.white,
@@ -57,6 +59,141 @@ class _AddMembersState extends State<AddMembers> {
   }
 
   Widget memberTile(int index) {
-    return Container();
+    return Slidable(
+      key: const ValueKey(0),
+      startActionPane: ActionPane(
+        motion: const ScrollMotion(),
+        // dismissible: DismissiblePane(onDismissed: () {print("object");}),
+        children:  [
+          SlidableAction(
+            onPressed: (context){
+              members.removeAt(index);
+              setState(() {
+                
+              });
+            },
+            borderRadius: BorderRadius.all(Radius.circular(5)),
+            backgroundColor: Color(0xFFFE4A49),
+            foregroundColor: Colors.white,
+            icon: Icons.delete,
+            label: 'Delete',
+          ),
+
+        ],
+      ),
+      child:  Container(
+          margin: EdgeInsets.symmetric(vertical: 5,horizontal: 25),
+          padding:EdgeInsets.symmetric(vertical: 5,horizontal: 15),
+          decoration: BoxDecoration(
+            borderRadius: BorderRadius.all(Radius.circular(8),),
+                border: Border.all(width: 2,color: CustomColors.primaryColor)
+          ),
+          child: Row(
+            children: [
+              Text(members[index].name,style: TextStyle(fontSize: 18),),
+              Spacer(),
+              Text("Age: ${members[index].age.toString()}"),
+            ],
+          ),
+        ),
+    );
   }
+
+  AddMember() {
+        var fs=TextStyle(fontSize: 20,);
+        TextEditingController tecName=TextEditingController();
+        TextEditingController tecAge=TextEditingController();
+        @override
+    void dispose() {
+      // Add code before the super
+      super.dispose();
+    }
+
+    showDialog(context: context,
+        builder: (context) => Dialog(
+          child: Container(
+            height: 200,
+            width: 200,
+          padding: EdgeInsets.symmetric(vertical: 15,horizontal: 20),
+          decoration: BoxDecoration(
+            borderRadius: BorderRadius.all(Radius.circular(25)),
+            border: Border.all(color: CustomColors.primaryColor,width: 2)
+          ),
+            child: Column(
+              crossAxisAlignment: CrossAxisAlignment.start,
+              children: [
+                Row(
+                  children: [
+                    Text("Name",style: fs,),
+                    Spacer(),
+                    SizedBox(
+                      width: 160,
+                      height: 40,
+                      child: TextField(controller: tecName,
+                        cursorHeight: 25,
+                        decoration: InputDecoration(
+                          contentPadding: EdgeInsets.all(5),
+                          border: OutlineInputBorder(
+                            borderRadius: BorderRadius.all(Radius.circular(10)),
+                            borderSide: BorderSide(width: 2)
+                          )
+                      ),),
+                    )
+                  ],
+                ),
+                Gap(10),
+                Row(
+                  children: [
+                    Text("Age",style: fs,),
+                    Gap(40),
+                    SizedBox(
+                      width: 50,
+                      height: 40,
+                      child: TextField(controller: tecAge,
+                        cursorHeight: 25,
+                        decoration: InputDecoration(
+                          contentPadding: EdgeInsets.all(5),
+                            border: OutlineInputBorder(
+                                borderRadius: BorderRadius.all(Radius.circular(10)),
+                                borderSide: BorderSide(width: 2)
+                            )
+                        ),),
+                    )
+                  ],
+                ),
+                Container(
+                  height: 50,
+                  width: MediaQuery.of(context).size.width-50,
+                  margin: EdgeInsets.only(bottom: 5,top: 20),
+                  child: ElevatedButton(
+                    onPressed: () {
+                      Member m=new Member(age: int.parse(tecAge.text), name: tecName.text);
+                      members.add(m);
+                      Navigator.pop(context);
+
+                    },
+                    style: ElevatedButton.styleFrom(
+                      elevation: 5,
+                      shape: const RoundedRectangleBorder(borderRadius: BorderRadius.all(Radius.circular(15))),
+                      backgroundColor: CustomColors.primaryColor,
+                      foregroundColor: Colors.white,
+                    ),
+                    child: const Text('Add Member',style: TextStyle(fontSize: 22),),
+                  ),
+                ),
+              ],
+            ),
+          
+          
+          ),
+        )
+    );
+  }
+}
+
+class Member{
+  final int age;
+ final String name;
+
+  Member({required this.age, required this.name});
 }
